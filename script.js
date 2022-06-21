bbd88eb7a7202f9f60519e13c36af5a6443e80e5
admin <admin@Sammies-Mac.local>
2022-06-21 23:11:41 +0100

    ready for hosting and testing

---
+++ script.js
@@ -0,0 +1,70 @@
+
+const form = document.getElementById("form");
+const messageContainer = document.querySelector(".message-container");
+const message = document.getElementById("message");
+
+
+
+
+let isValid = false;
+
+
+function validateForm(){
+
+  // Using constraint API
+  
+  isValid = form.checkValidity();
+  
+  console.log(isValid);
+  
+  // style main message for an error
+if (isValid === true ) {
+
+  message.textContent = " Message sent";
+  message.style.color = "green";
+  messageContainer.style.borderColor = "green";
+  
+  
+} else {
+  message.textContent = "Please fill the values";
+  message.style.color = "red";
+  messageContainer.style.borderColor = "red";
+  
+  
+}
+
+ 
+  }
+
+function processFormData(e){
+
+  e.preventDefault();
+
+  // validate form
+  validateForm();
+
+}
+
+// event listener
+form.addEventListener("submit", processFormData);
+
+
+
+
+
+
+  AOS.init();
+
+  AOS.init({
+
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+  });
